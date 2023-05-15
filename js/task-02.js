2e3f2f94cbb697a2421367686a314766711abd6e
Pavlo Honcharenko <pavelgonchar13@gmail.com>
2023-05-15 18:10:53 +0300

    task-2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,12 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const textElement = document.querySelector("#ingredients");
+textElement.classList.add('item');
+
+for (let i = 1; i < ingredients.length; i ++) {
+  let li = document.createElement("li");
+  li.innerHTML = ingredients[i];
+  textElement.append(li);
+}
