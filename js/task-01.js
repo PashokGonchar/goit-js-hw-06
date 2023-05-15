36407610ee505cbc98614fc1d7c71779e7b6a593
Pavlo Honcharenko <pavelgonchar13@gmail.com>
2023-05-15 14:56:59 +0300

    task-1

---
+++ js/task-01.js
@@ -0,0 +1,10 @@
+const categories = document.querySelectorAll('.item');
+
+let numbersArr = categories.length;
+console.log("Number of Categories : ", numbersArr);
+
+categories.forEach((cat) => {
+  console.log('Category:', cat.querySelector('h2').textContent);
+  console.log('Elements:', cat.querySelectorAll('li').length);
+});
+
