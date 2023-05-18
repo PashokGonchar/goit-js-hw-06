c8069bdf381ad93d5bea7083a9972d298ec642f7
Pavlo Honcharenko <pavelgonchar13@gmail.com>
2023-05-18 23:23:48 +0300

    task-6

---
+++ js/task-06.js
@@ -0,0 +1,7 @@
+const inputTextEl = document.getElementById('validation-input');
+
+inputTextEl.addEventListener('blur', (e) => {
+    e.target.value.length == inputTextEl.dataset.length
+    ? (inputTextEl.className = 'valid')
+    : (inputTextEl.className = 'invalid')
+});
