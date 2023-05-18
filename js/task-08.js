790a7370642da363cf4ca3d0d60b5d3f96ee7b98
Pavlo Honcharenko <pavelgonchar13@gmail.com>
2023-05-19 00:32:07 +0300

    task-8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const formEl = document.querySelector('.login-form');
+const inputsEl = formEl.elements;
 
+let loginForm = {}
+
+formEl.addEventListener('submit', (e) => {
+    e.preventDefault();
+    if (inputsEl['email'].value === '' || inputsEl['password'].value === '') {
+      alert('Всі поля повинні бути заповнені!!!');
+    } else {
+        loginForm['email'] = inputsEl['email'].value;
+        loginForm['password'] = inputsEl['password'].value;
+        console.log(loginForm);
+        formEl.reset();
+    } 
+    return false;  
+});
