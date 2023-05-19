39db007dcac945d7231a9da4a44b3e9cdfee2886
Pavlo Honcharenko <pavelgonchar13@gmail.com>
2023-05-20 02:16:28 +0300

    task-8

--- js/task-01.js
+++ js/task-01.js
@@ -1,10 +1,9 @@
 const categories = document.querySelectorAll('.item');
 
 let numbersArr = categories.length;
-console.log("Number of Categories : ", numbersArr);
+console.log('Number of Categories : ', numbersArr);
 
 categories.forEach((cat) => {
   console.log('Category:', cat.querySelector('h2').textContent);
   console.log('Elements:', cat.querySelectorAll('li').length);
 });
-
--- js/task-08.js
+++ js/task-08.js
@@ -1,19 +1,19 @@
 const formEl = document.querySelector('.login-form');
 const inputsEl = formEl.elements;
 
-let loginForm = {}
+let loginForm = {};
 
-function correctForm () {
-    e.preventDefault();
-    if (inputsEl['email'].value === '' || inputsEl['password'].value === '') {
-      alert('Всі поля повинні бути заповнені!!!');
-    } else {
-        loginForm['email'] = inputsEl['email'].value;
-        loginForm['password'] = inputsEl['password'].value;
-        console.log(loginForm);
-        formEl.reset();
-    } 
-    return false;  
+function correctForm(e) {
+  e.preventDefault();
+  if (inputsEl['email'].value === '' || inputsEl['password'].value === '') {
+    alert('Всі поля повинні бути заповнені!!!');
+  } else {
+    loginForm['email'] = inputsEl['email'].value;
+    loginForm['password'] = inputsEl['password'].value;
+    console.log(loginForm);
+    formEl.reset();
+  }
+  return false;
 }
 
 formEl.addEventListener('submit', correctForm);
