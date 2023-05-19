4178791128d18f58139d29009226fa2f9ede003e
Pavlo Honcharenko <pavelgonchar13@gmail.com>
2023-05-20 00:48:02 +0300

    task-2

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,37 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const createBtn = document.querySelector('[data-create]');
+const destroyBtn = document.querySelector('[data-destroy]');
+let inputEl = document.getElementById('controls').firstElementChild;
+let boxesEl = document.getElementById('boxes');
+
+function createBoxes() {
+  let amount = inputEl.value;
+  if (amount > 0) {
+    boxesEl.innerHTML = '';
+    let sizeDiv = 30;
+    for (let i = 0; i < amount; i++) {
+      let boxEl = document.createElement('div');
+      boxesEl.appendChild(boxEl);
+      boxEl.style.width = `${sizeDiv}px`;
+      boxEl.style.height = `${sizeDiv}px`;
+      boxEl.style.border = '1px solid red';
+      sizeDiv += 10;
+      boxEl.style.backgroundColor = getRandomHexColor();
+    }
+  }
+}
+createBtn.addEventListener('click', createBoxes);
+
+inputEl.addEventListener('keyup', (e) => {
+  e.target.value = 
+    e.target.value > +e.target.max ? +e.target.max : e.target.value;
+});
+
+function destroyBoxes() {
+  boxesEl.innerHTML = '';
+}
+
+destroyBtn.addEventListener('click', destroyBoxes);
